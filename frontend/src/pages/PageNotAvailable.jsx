6a44d4fb4cecf2e37f1051fb3d3ff0c8702067ea
httpsaelfricEzekiel <ezekielnii123@gmail.com>
2025-01-18 20:26:44 -0800

    added routing, home, about and login components

---
+++ frontend/src/pages/PageNotAvailable.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+function PageNotAvailable() {
+    return (
+        <div>
+            <h1>Page Not Available</h1>
+            
+            <Link to="/ICMS">Go to Home</Link>
+        </div>
+    )
+}
+
+export default PageNotAvailable;
