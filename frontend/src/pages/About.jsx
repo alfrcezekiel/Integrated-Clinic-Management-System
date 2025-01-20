76ace377ee60a22b951e3fa64956c172d7ab50d4
httpsaelfricEzekiel <ezekielnii123@gmail.com>
2025-01-20 19:03:58 +0800

    added a background image in home component

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 function About(){
+    useEffect(() => {
+        document.title = "About";
+    }, []);
+    
     return (
         <div>
             <h1>About Integrated Clinic Management System</h1>
