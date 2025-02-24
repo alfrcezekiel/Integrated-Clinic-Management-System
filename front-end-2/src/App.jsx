f6f0fde9d3e3f84186955268e7c904e7094166a0
alfrcezekiel <ezekielnii123@gmail.com>
2025-02-24 12:50:37 +0800

    added new template for project

---
+++ front-end-2/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import LandingPageHeader from './components/LandingPageHeader.jsx'
+import MainContent from './components/MainContent.jsx'
+
+function App() {
+  return (
+    <>
+      <LandingPageHeader />
+      <MainContent /> 
+    </>
+  )
+}
+
+export default App
