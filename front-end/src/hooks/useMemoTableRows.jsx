7667782aaa5225af104557afa1c3061a9e8c560a
alfrcezekiel <ezekielnii123@gmail.com>
2025-03-17 19:40:18 +0800

    fixed date format in doctor's dashboard

--- front-end/src/hooks/useMemoTableRows.jsx
+++ front-end/src/hooks/useMemoTableRows.jsx
@@ -13,7 +13,7 @@ const formatDate = (dateString) => {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
     const day = String(date.getDate()).padStart(2, '0');
-    return `${month}/${day}/${year}`;
+    return `${year}-${month}-${day}`;
 };
 
 // function to determine  the color of patients status
--- front-end/src/layouts/doctor-utils/DoctorsTablesListOfAppointment.jsx
+++ front-end/src/layouts/doctor-utils/DoctorsTablesListOfAppointment.jsx
@@ -108,7 +108,7 @@ const DoctorsTablesListOfAppointments = () => {
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
         const day = String(date.getDate()).padStart(2, '0');
-        return `${month}/${day}/${year}`;
+        return `${year}-${month}-${day}`;
     };
 
     // this function is used to update the appointment details
@@ -118,7 +118,7 @@ const DoctorsTablesListOfAppointments = () => {
             if(formData.status === "Scheduled"){
                 formData.appointmentDate = new Date(formData.appointmentDate).toISOString().split("T")[0];
             }
-            
+
             const response = await CMS.put(`/CMS/doctors-dashboard/updateAppointment/${formData.appointmentID}`, formData);
 
             if (response.status === 200) {
