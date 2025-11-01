a3dc040423fcf9ae3650dd13f31dee734aca8b06
alfrcezekiel <ezekielnii123@gmail.com>
2025-11-01 09:28:53 +0800

    fix: main file and user routes in backend for cors error

--- back-end/main.js
+++ back-end/main.js
@@ -170,8 +170,6 @@ const corsOptions = {
     maxAge: 86400 // Cache preflight for 24 hours
 }
 
-// route for CMS
-app.use("/", cms);
 app.use(cors(corsOptions));
 
 // error handling for server error
@@ -206,6 +204,9 @@ app.use("/uploads/clinic_images", express.static(clinicImagesPath));
 
 const medicalReportPath = path.join(__dirname, "uploads/medical_reports");
 app.use("/uploads/medical_reports", express.static(medicalReportPath));
+
+// route for CMS
+app.use("/", cms);
 
 app.disable("etag");
 
